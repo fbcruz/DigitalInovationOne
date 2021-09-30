4184410b90137a43c56a8d28895c0f536f974de9
Franciele Barros <fbcruz@outlook.com.br>
2021-09-29 22:20:15 -0300

    Criando um dominio utilizando os pilares da orientacao a objetos

---
+++ AgenciaBancaria/AgenciaBancaria.App/Program.cs
@@ -0,0 +1,31 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("Rua teste", "123456", "Barra Bonita", "SP");
+                Cliente cliente = new Cliente("Franciele", "123456789", "123456", endereco);
+
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+
+                Console.WriteLine("Conta Criada: " + conta.Situacao + ":" + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                conta.Abrir("abc123");
+
+                Console.WriteLine("Conta Criada: " + conta.Situacao + ":" + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
